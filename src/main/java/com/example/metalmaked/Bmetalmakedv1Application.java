d192cd35a5a18cf7a66244ac7a820c47b57d0520
Sena <dimatesebas73@gmail.com>
2025-03-01 09:18:21 -0500

    intalacion de dependencias y creacion de estructura

---
+++ src/main/java/com/example/metalmaked/Bmetalmakedv1Application.java
@@ -0,0 +1,13 @@
+package com.example.metalmaked;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bmetalmakedv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bmetalmakedv1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/metalmaked/Bmetalmakedv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.metalmaked;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bmetalmakedv1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
